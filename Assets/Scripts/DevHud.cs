d7fdba5a740689de240b6c1b5ba5c44e1e66516e
Jehuty-Z <jehuty@studio-surlalune.com>
2023-12-28 12:44:16 -0500

    Added 2 scripts (developer hud and dynamic resolution controller).

---
+++ Assets/Scripts/DevHud.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DevHud : MonoBehaviour
+{
+    private readonly Color kWhite = new Color(1.0f, 1.0f, 1.0f, 0.66f);
+    private readonly Color kLightGrey = new Color(0.66f, 0.66f, 0.66f, 0.66f);
+    private readonly Color kDarkGrey = new Color(0.33f, 0.33f, 0.33f, 0.66f);
+    private Text gpuTimeText;
+    private Text cpuTimeText;
+    private Text frameTimeText;
+    private Canvas canvas;
+
+    private Image refResolutionBox;
+    private Image scaledResolutionBox;
+    private Text refResolutionText;
+    private Text scaledResolutionText;
+    private Text scaledPercentText;
+
+    private FrameTiming[] timings = new FrameTiming[1];
+
+    void Start()
+    {
+        // Create Canvas
+        GameObject canvasObject = new GameObject("DeveloperHud Canvas");
+        canvas = canvasObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+        canvasObject.AddComponent<CanvasScaler>();
+        canvasObject.AddComponent<GraphicRaycaster>();
+
+        gpuTimeText = CreateText(canvas, "gpuTime", 10, -10);
+        cpuTimeText = CreateText(canvas, "cpuTime", 10, -30);
+        frameTimeText = CreateText(canvas, "frameTime", 10, -50);
+
+        refResolutionBox = CreateBox(canvas, "resolutionBox", 150, -10);
+        int displayWidth = Screen.width;
+        int displayHeight = Screen.height;
+        refResolutionBox.rectTransform.sizeDelta = new Vector2(displayWidth/16, displayHeight/16);
+        refResolutionBox.color = kLightGrey;
+        scaledResolutionBox = CreateBox(canvas, "scaledResolutionBox", 150, -10, false);
+        scaledResolutionBox.rectTransform.sizeDelta = new Vector2(displayWidth/16.0f, displayHeight/16.0f);
+        refResolutionText = CreateText(canvas, "refRes", 150 + displayWidth/16.0f + 10, -10);
+        refResolutionText.color = kLightGrey;
+        scaledResolutionText = CreateText(canvas, "scaledRes", 150 + displayWidth/16.0f + 10, -30);
+        scaledPercentText = CreateText(canvas, "scaledPercent", 150 + 5, -10 - 5);
+        scaledPercentText.color = kDarkGrey;
+    }
+
+    void Update()
+    {
+        int displayWidth = Screen.width;
+        int displayHeight = Screen.height;
+        refResolutionText.text = $"res:{displayWidth}x{displayHeight}";
+
+        if (FrameTimingManager.GetLatestTimings(1, timings) != 1)
+        {
+            gpuTimeText.text = $"GPU: <?>";
+            cpuTimeText.text = $"CPU: <?>";
+            frameTimeText.text = $"Frame: <?>";
+            scaledResolutionText.text = "dyn:<?>x<?>";
+            return;
+        }
+
+        double gpuTime = timings[0].gpuFrameTime;
+        double cpuTime = timings[0].cpuMainThreadFrameTime;
+        double frameTime = timings[0].cpuFrameTime;
+        float scaledWidth = (float)timings[0].widthScale;
+        float scaledHeight = (float)timings[0].heightScale;
+        int sw = (int)(displayWidth*scaledWidth);
+        int sh = (int)(displayHeight*scaledHeight);
+
+        gpuTimeText.text = $"GPU: {gpuTime.ToString("F2")}ms";
+        cpuTimeText.text = $"CPU: {cpuTime.ToString("F2")}ms (?)"; // CPU time cannot always be trusted
+        frameTimeText.text = $"Frame: {frameTime.ToString("F2")}ms";
+        scaledResolutionBox.rectTransform.sizeDelta = new Vector2(sw/16.0f, sh/16.0f);
+        scaledResolutionText.text = $"dyn:{sw}x{sh}";
+        scaledPercentText.text = $"{ 100 * (sw*sh) / (displayWidth*displayHeight) }%";
+    }
+
+    private Text CreateText(Canvas canvas, string name, float x, float y)
+    {
+        GameObject textObject = new GameObject(name);
+        textObject.transform.SetParent(canvas.transform);
+        Text text = textObject.AddComponent<Text>();
+        text.color = kWhite;
+        text.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+        text.fontSize = 12;
+        text.rectTransform.anchorMin = new Vector2(0, 1); // Top left anchor
+        text.rectTransform.anchorMax = new Vector2(0, 1); // Top left anchor
+        text.rectTransform.pivot = new Vector2(0, 1); // Top left pivot
+        text.rectTransform.anchoredPosition = new Vector2(x, y);
+        text.rectTransform.sizeDelta = new Vector2(300, 100);
+        // Add outline to Text
+        Outline textOutline = textObject.AddComponent<Outline>();
+        textOutline.effectColor = kDarkGrey; // outline color
+        textOutline.effectDistance = new Vector2(1, -1); // outline offset
+        return text;
+    }
+
+    private Image CreateBox(Canvas canvas, string name, float x, float y, bool outline = true)
+    {
+        GameObject barObject = new GameObject("name");
+        barObject.transform.SetParent(canvas.transform);
+        Image box = barObject.AddComponent<Image>();
+        box.color = kWhite;
+        box.rectTransform.anchorMin = new Vector2(0, 1); // Top left anchor
+        box.rectTransform.anchorMax = new Vector2(0, 1); // Top left anchor
+        box.rectTransform.pivot = new Vector2(0, 1); // Top left pivot
+        box.rectTransform.anchoredPosition = new Vector2(x, y);
+        box.rectTransform.sizeDelta = new Vector2(100, 20);
+        if (outline)
+        {
+            // Add outline
+            Outline boxOutline = barObject.AddComponent<Outline>();
+            boxOutline.effectColor = kDarkGrey; // outline color
+            boxOutline.effectDistance = new Vector2(1, -1); // outline offset
+        }
+        return box;
+    }
+}
---
+++ Assets/Scripts/DynamicResController.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DynamicResController : MonoBehaviour
+{
+    /// Exponential Moving Average (EMA) on the last 8 frames.
+    private const int kEMAFrameCount = 8;
+    private const int kTileSize = 32;
+    /// Target frame time in milliseconds.
+    private static float targetFrameTimeMS = 32.0f;
+    /// delta time to wiggle around in milliseconds.
+    private static float targetFrameDeltaMS = 1.0f;
+    /// EMA gpu time (without present).
+    private static float emaGpuTimeMS;
+    /// We may skip some frame to decide dynamic resolution in order to slow-down the logic.
+    private static int skipFrameCounter = 0;
+    /// Dynamic resolution width in pixel. Always a multiple of 32.
+    /// To simplify calculation, it is not clamped to the display buffer min/max resolutions.
+    private static int scaledWidth;
+    /// Dynamic resolution height in pixel. Always a multiple of 32.
+    /// To simplify calculation, it is not clamped to the display buffer min/max resolutions.
+    private static int scaledHeight;
+    /// Avoid dynamic allocation every frame.
+    private static FrameTiming[] timings = new FrameTiming[1];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        emaGpuTimeMS = targetFrameTimeMS;
+        scaledWidth = ((Screen.width + kTileSize - 1) / kTileSize) * kTileSize;
+        scaledHeight = ((Screen.height + kTileSize - 1) / kTileSize) * kTileSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (skipFrameCounter > 0)
+        {
+            --skipFrameCounter;
+            return;
+        }
+
+        int width = Screen.width;
+        int height = Screen.height;
+
+        if (FrameTimingManager.GetLatestTimings(1, timings) == 1
+         // some platforms may give garbage times at start-up...
+         && timings[0].gpuFrameTime >= 0.0 && timings[0].gpuFrameTime <= 1000.0)
+        {
+            float k = 2.0f / (kEMAFrameCount + 1.0f);
+            emaGpuTimeMS = (float)timings[0].gpuFrameTime*k + emaGpuTimeMS*(1-k);
+
+            if (emaGpuTimeMS > targetFrameTimeMS + 3.0f * targetFrameDeltaMS)
+            {
+                scaledWidth -= kTileSize;
+                scaledHeight -= kTileSize;
+                skipFrameCounter = 0;
+            }
+            else if (emaGpuTimeMS > targetFrameTimeMS + targetFrameDeltaMS)
+            {
+                scaledWidth -= kTileSize;
+                scaledHeight -= kTileSize;
+                skipFrameCounter = 3;
+            }
+            else if (emaGpuTimeMS < targetFrameTimeMS - targetFrameDeltaMS)
+            {
+                scaledWidth += kTileSize;
+                scaledHeight += kTileSize;
+                skipFrameCounter = 3;
+            }
+            else if (emaGpuTimeMS < targetFrameTimeMS - 3.0f * targetFrameDeltaMS)
+            {
+                scaledWidth += kTileSize;
+                scaledHeight += kTileSize;
+                skipFrameCounter = 0;
+            }
+        }
+        
+        float w = Mathf.Clamp(scaledWidth, width * 0.5f, width * 1.0f);
+        float h = Mathf.Clamp(scaledHeight, height * 0.5f, height * 1.0f);
+
+        ScalableBufferManager.ResizeBuffers(w/width, h/height);
+        //Debug.Log($"scaledWidth={scaledWidth} scaledHeight={scaledHeight} ema_gpuTime={emaGpuTimeMS}\n");
+    }
+}
