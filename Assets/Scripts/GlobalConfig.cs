dfcd0801ab94398a0324f6228750200bdefed270
Jehuty-Z <jehuty@studio-surlalune.com>
2024-01-16 21:12:47 -0500

    Fixed dynamic resolution bug.

--- Assets/Scripts/GlobalConfig.cs
+++ Assets/Scripts/GlobalConfig.cs
@@ -3,9 +3,9 @@ public sealed class GlobalConfig
     public static readonly DevHud.Layout DevHudLayout = DevHud.Layout.BottomLeft;
 
     /// For dynamic resolution control.
-    public static readonly float TargetFrameTimeMS = 0.032f; // 32 milliseconds
+    public static readonly float TargetFrameTimeMS = 32f; // 32 milliseconds
 
     /// For dynamic resolution control.
-    public static readonly float TargetFrameDeltaMS = 0.001f; // 1 millisecond
+    public static readonly float TargetFrameDeltaMS = 1f; // 1 millisecond
     
 }
